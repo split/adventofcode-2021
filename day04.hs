9ab2aaaffa0c0ef8a51fbe4d43ebf6451cbfb537
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-04 13:09:12 +0200

    day04 solutions

---
+++ day04.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE TupleSections #-}
+
+import Control.Monad (ap)
+import Data.List (find, transpose)
+import Data.List.Split (splitOn)
+import Data.Maybe (catMaybes)
+
+type Card = [[Int]]
+
+type Drawn = [Int]
+
+main :: IO ()
+main = interact (unlines . sequence [part1, part2] . parseInput)
+
+part1, part2 :: ([Card], Drawn) -> String
+part1 = ("Part 1: " ++) . show . head . uncurry playBingo
+part2 = ("Part 2: " ++) . show . last . uncurry playBingo
+
+playBingo :: [Card] -> Drawn -> [Int]
+playBingo cards drawn = map score $ catMaybes $ zipWith (\round n -> (,n) <$> find isWinner round) rounds drawn
+  where
+    rounds = drop 1 $ scanl (markDrawn . filter (not . isWinner)) cards drawn
+    markDrawn cards n = map (map (map (\x -> if x == n then - 1 else x))) cards
+    isWinner = elem 0 . map (length . filter (>= 0)) . ap (++) transpose
+    score (card, n) = n * sum (filter (> 0) $ concat card)
+
+parseInput :: String -> ([Card], Drawn)
+parseInput = parse . splitOn "\n\n"
+  where
+    parse (drawn : cards) = (map parseCard cards, read <$> splitOn "," drawn)
+    parseCard = map (map read . words) . lines
