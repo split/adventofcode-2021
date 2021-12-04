c785545c6d05241aa155e17ee5ae1b8c7e8e8c23
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-04 15:36:55 +0200

    day04: remove hack of using negative for marked

--- day04.hs
+++ day04.hs
@@ -5,7 +5,7 @@ import Data.List (find, transpose)
 import Data.List.Split (splitOn)
 import Data.Maybe (catMaybes)
 
-type Card = [[Int]]
+type Card = [[(Int, Bool)]]
 
 type Drawn = [Int]
 
@@ -20,12 +20,12 @@ playBingo :: [Card] -> Drawn -> [Int]
 playBingo cards drawn = map score $ catMaybes $ zipWith (\round n -> (,n) <$> find isWinner round) rounds drawn
   where
     rounds = drop 1 $ scanl (markDrawn . filter (not . isWinner)) cards drawn
-    markDrawn cards n = map (map (map (\x -> if x == n then - 1 else x))) cards
-    isWinner = elem 0 . map (length . filter (>= 0)) . ap (++) transpose
-    score (card, n) = n * sum (filter (> 0) $ concat card)
+    markDrawn cards n = map (map (map (\(n', b) -> (n', b || n' == n)))) cards
+    isWinner = any (all snd) . ap (++) transpose
+    score (card, n) = n * sum [n' | (n', b) <- concat card, not b]
 
 parseInput :: String -> ([Card], Drawn)
 parseInput = parse . splitOn "\n\n"
   where
     parse (drawn : cards) = (map parseCard cards, read <$> splitOn "," drawn)
-    parseCard = map (map read . words) . lines
+    parseCard = map (map ((,False) . read) . words) . lines
