f70f79a9d3d86b3be5be148742d8e64f3022e451
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-05 15:44:07 +0200

    day05: cleanup move parsing to the end

--- day05.hs
+++ day05.hs
@@ -14,12 +14,6 @@ part1, part2 :: [Line] -> [Char]
 part1 = ("Part 1: " ++) . show . S.size . overlap . map points . filter isDiagonal
 part2 = ("Part 2: " ++) . show . S.size . overlap . map points
 
-parse :: String -> Line
-parse = toLine . map (toPoint . map read . splitOn ",") . splitOn " -> "
-  where
-    toPoint [x, y] = (x, y)
-    toLine [start, end] = Line start end
-
 overlap :: Ord a => [Set a] -> Set a
 overlap lines = S.unions [S.intersection p1 p2 | (p1 : ps) <- tails lines, p2 <- ps]
 
@@ -30,3 +24,9 @@ points (Line (x1, y1) (x2, y2)) = S.fromList $ zip (range x1 x2) (range y1 y2)
 
 isDiagonal :: Line -> Bool
 isDiagonal (Line (x1, y1) (x2, y2)) = x1 == x2 || y1 == y2
+
+parse :: String -> Line
+parse = toLine . map (toPoint . map read . splitOn ",") . splitOn " -> "
+  where
+    toPoint [x, y] = (x, y)
+    toLine [start, end] = Line start end
