fe6241d5cdf3de91395ecb116c0bdfaa927ae4cf
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-07 16:55:39 +0200

    day07: adjust mean to tip rounding to right direction

--- day07.hs
+++ day07.hs
@@ -3,20 +3,21 @@ import Data.List
 import Data.List.Split
 
 main :: IO ()
-main = interact (unlines . sequence [part1, part2] . map read . splitOn ",")
+main = interact (unlines . sequence [part1, part2] . sort . map read . splitOn ",")
 
 part1, part2 :: [Int] -> [Char]
 part1 = ("Part 1: " ++) . show . ap (flip $ sumCost id) median
-part2 = ("Part 2: " ++) . show . ap (best . flip (sumCost increasingCost)) mean
+part2 = ("Part 2: " ++) . show . ap (flip $ sumCost increasingCost) mean
   where
-    best f m = minimum $ map f [floor m, ceiling m]
     increasingCost x = x * (x + 1) `div` 2
 
 sumCost :: (Int -> Int) -> Int -> [Int] -> Int
 sumCost cost to = sum . map (cost . abs . subtract to)
 
-median :: [Int] -> Int
-median xs = sort xs !! (length xs `div` 2 - 1)
-
-mean :: (Fractional b, Real a) => [a] -> b
-mean xs = realToFrac (sum xs) / genericLength xs
+median, mean :: [Int] -> Int
+median xs = xs !! (length xs `div` 2 - 1)
+mean xs = round (value + (count - 2 * countLessThan value) / (2 * count))
+  where
+    value = realToFrac (sum xs) / count
+    countLessThan x = genericLength $ takeWhile ((< x) . fromIntegral) xs
+    count = genericLength xs
