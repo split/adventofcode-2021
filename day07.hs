91f3c9545b1a4680cf651040dc5bc3c856d015f5
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-07 10:09:22 +0200

    day07 solutions

---
+++ day07.hs
@@ -0,0 +1,13 @@
+import Data.List.Split
+
+main :: IO ()
+main = interact (unlines . sequence [part1, part2] . map read . splitOn ",")
+
+part1, part2 :: [Int] -> [Char]
+part1 = ("Part 1: " ++) . show . minCost id
+part2 = ("Part 2: " ++) . show . minCost (costs !!)
+  where
+    costs = scanl1 (+) [0 ..]
+
+minCost :: (Int -> Int) -> [Int] -> Int
+minCost cost xs = minimum $ map sum $ [map (cost . abs . subtract a) xs | a <- xs]
