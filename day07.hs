a6e4362dcd96109831bd8fdf0df809aa6c48507f
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-07 10:09:48 +0200

    day07 use gauss for second part cost

--- day07.hs
+++ day07.hs
@@ -5,9 +5,9 @@ main = interact (unlines . sequence [part1, part2] . map read . splitOn ",")
 
 part1, part2 :: [Int] -> [Char]
 part1 = ("Part 1: " ++) . show . minCost id
-part2 = ("Part 2: " ++) . show . minCost (costs !!)
+part2 = ("Part 2: " ++) . show . minCost increasingCost
   where
-    costs = scanl1 (+) [0 ..]
+    increasingCost x = x * (x + 1) `div` 2
 
 minCost :: (Int -> Int) -> [Int] -> Int
 minCost cost xs = minimum $ map sum $ [map (cost . abs . subtract a) xs | a <- xs]
