cdb9600dabb7dc39569f374d14a5cb6832f8ac02
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-10 21:03:34 +0200

    day10 swap maybe to either

--- day10.hs
+++ day10.hs
@@ -1,24 +1,23 @@
+import Data.Either
 import Data.List (elemIndex, sort)
-import Data.Maybe (isNothing, mapMaybe)
+import Data.Maybe (mapMaybe)
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . map (parse "") . lines)
 
-part1, part2 :: [(Maybe Char, String)] -> [Char]
-part1 = ("Part 1: " ++) . show . sum . mapMaybe (`lookup` scores) . mapMaybe fst
-part2 = ("Part 2: " ++) . show . middle . sort . map (sumScore . snd) . filter (isNothing . fst)
+part1, part2 :: [Either Char String] -> [Char]
+part1 = ("Part 1: " ++) . show . sum . mapMaybe (`lookup` scores) . lefts
+part2 = ("Part 2: " ++) . show . middle . sort . map sumScore . rights
 
-parse :: String -> String -> (Maybe Char, String)
+parse :: String -> String -> Either Char String
 parse s ('(' : ix) = parse (')' : s) ix
 parse s ('[' : ix) = parse (']' : s) ix
 parse s ('{' : ix) = parse ('}' : s) ix
 parse s ('<' : ix) = parse ('>' : s) ix
-parse s "" = (Nothing, s)
+parse s "" = Right s
 parse (x : xs) (i : ix)
-  | x == i = next
-  | otherwise = (Just i, snd next)
-  where
-    next = parse xs ix
+  | x == i = parse xs ix
+  | otherwise = Left i
 
 scores :: [(Char, Int)]
 scores = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
