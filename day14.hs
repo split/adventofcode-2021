e1cf3064aa02c12292f493d649b8deb43b213004
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-14 23:44:58 +0200

    day14: refactor and simplify solutions

--- day14.hs
+++ day14.hs
@@ -3,52 +3,41 @@ import Data.Foldable (Foldable (foldr'))
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (fromMaybe)
 
 type PolyPair = (Char, Char)
 
-type PolyCount = Map Char Int
+type PolymerCount = Map PolyPair Int
+
+type MonomerCount = Map Char Int
 
 type PolymerMap = Map PolyPair Char
 
 main :: IO ()
-main = interact (unlines . sequence [part1, part2] . parse)
+main = interact (unlines . sequence [part1, part2] . uncurry polyCounts . parse)
 
-part1, part2 :: (String, PolymerMap) -> String
-part1 = ("Part 1: " ++) . show . diffCommon . uncurry (polyCounts 10)
-part2 = ("Part 2: " ++) . show . diffCommon . uncurry (polyCounts 40)
+part1, part2 :: [PolymerCount] -> String
+part1 = ("Part 1: " ++) . show . diffCommon . monomerCount . (!! 10)
+part2 = ("Part 2: " ++) . show . diffCommon . monomerCount . (!! 40)
 
-diffCommon :: PolyCount -> Int
+diffCommon :: Map a Int -> Int
 diffCommon = ap ((-) . maximum) minimum . M.elems
 
-polyCounts :: Int -> String -> Map PolyPair Char -> PolyCount
-polyCounts n template rules = mergeCounts $ counts template : map (polyCount n) (polyPairs template)
+polyCounts :: PolymerMap -> [PolyPair] -> [PolymerCount]
+polyCounts rules = polyCounts' . counts
   where
-    polyCounts' n' = map (\n'' -> M.mapWithKey (const . polyCount n'') rules) [0 ..] !! n'
-    polyCount :: Int -> PolyPair -> PolyCount
-    polyCount 0 _ = M.empty
-    polyCount n' p@(a, b) = case p `M.lookup` rules of
-      Nothing -> M.empty
-      Just c ->
-        mergeCounts
-          [ M.singleton c 1,
-            (a, c) `polyLookup` polyCounts' (n' - 1),
-            (c, b) `polyLookup` polyCounts' (n' - 1)
-          ]
+    polyCounts' pc = pc : polyCounts' (M.fromListWith (+) $ concatMap (uncurry polymerization) (M.toList pc))
+    polymerization p@(a, b) count = let c = rules M.! p in [((a, c), count), ((c, b), count)]
 
-mergeCounts :: [PolyCount] -> PolyCount
-mergeCounts = M.unionsWith (+)
+monomerCount :: PolymerCount -> MonomerCount
+monomerCount = M.mapKeysWith (+) snd
 
-counts :: String -> PolyCount
+counts :: (Ord a) => [a] -> Map a Int
 counts = foldr' (flip (M.insertWith (+)) 1) M.empty
 
-parse :: String -> (String, PolymerMap)
+parse :: String -> (PolymerMap, [PolyPair])
 parse = parseChunks . splitOn "\n\n"
   where
-    parseChunks (template : pairs : _) = (template, parsePolymerPairs pairs)
+    parseChunks (template : pairs : _) = (parsePolymerPairs pairs, polyPairs template)
     parsePolymerPairs = foldr' (parsePolymerPair . splitOn " -> ") M.empty . lines
     parsePolymerPair ((a : b : _) : (c : _) : _) = M.insert (a, b) c
-
-polyPairs = ap zip tail
-
-polyLookup = (fromMaybe M.empty .) . M.lookup
+    polyPairs = ap zip tail
