a8d1240031602b2bf2046c64f3cb55f2fd432c3d
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2021-12-23 00:58:25 +0200

    day21 solutions

--- day21.hs
+++ day21.hs
@@ -1,20 +1,27 @@
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
 import Data.Char (digitToInt)
 import Data.Foldable
+import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Set (Set)
-import qualified Data.Set as S
-
-main = interact (unlines . sequence [part1] . map (player . last) . lines)
 
 type Player = (Int, Int)
 
+type Wins = (Int, Int)
+
 type Turn = (Player, Player, Int)
 
-part1 :: [(Int, Int)] -> String
-part1 = ("Part 1: " ++) . show . play . startTurn
+type Multiverse = Map (Player, Player) Wins
 
+main = interact (unlines . sequence [part1, part2] . map (player . last) . lines)
+
+part1, part2 :: [(Int, Int)] -> String
+part1 = ("Part 1: " ++) . show . play . startTurnCounter
+part2 = ("Part 2: " ++) . show . uncurry max . snd . dirac M.empty . startTurn
+
+play :: Turn -> Int
 play = maybe 0 looser . find (hasWinner 1000) . iterate turn
   where
     looser r = throwCount r * minimum (scores r)
@@ -28,16 +35,34 @@ turn ((pos, totalScore), next, throws) =
 roll3 :: Int -> Int
 roll3 throw = let roll = rotating 100 . (throw +) in roll 1 + roll 2 + roll 3
 
+dirac :: Multiverse -> (Player, Player) -> (Multiverse, Wins)
+dirac multiverse turn@((p1, s1), p2s@(_, s2))
+  | s1 >= 21 = (multiverse, (1, 0))
+  | s2 >= 21 = (multiverse, (0, 1))
+  | otherwise = case multiverse M.!? turn of
+    Just wins -> (multiverse, wins)
+    Nothing ->
+      let (multiverse', wins) = foldl go (multiverse, (0, 0)) chances
+       in (M.insert turn wins multiverse', wins)
+  where
+    go (multiverse, (w1, w2)) (score, count) =
+      let pos' = rotating 10 (p1 + score)
+          (multiverse', (w2', w1')) = dirac multiverse (p2s, (pos', s1 + pos'))
+       in (multiverse', (w1 + count * w1', w2 + count * w2'))
+
+rotating :: Integral a => a -> a -> a
 rotating n value = ((value - 1) `rem` n) + 1
 
 player s = (digitToInt s, 0)
 
-startTurn (p1 : p2 : _) = (p1, p2, 0)
+startTurn (p1 : p2 : _) = (p1, p2)
+
+startTurnCounter (p1 : p2 : _) = (p1, p2, 0)
 
 -- >>> chances
 -- fromList [(3,1),(4,3),(5,6),(6,7),(7,6),(8,3),(9,1)]
-chances :: Set (Int, Int)
-chances = let c = [1, 2, 3] in S.fromList $ M.toList $ M.fromListWith (+) [(t1 + t2 + t3, 1) | t1 <- c, t2 <- c, t3 <- c]
+chances :: [(Int, Int)]
+chances = let c = [1, 2, 3] in M.toList $ M.fromListWith (+) [(t1 + t2 + t3, 1) | t1 <- c, t2 <- c, t3 <- c]
 
 hasWinner :: Int -> Turn -> Bool
 hasWinner target = (>= target) . maximum . scores
